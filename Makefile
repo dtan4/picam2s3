468ec4d81bf48a19b0d94ccde72c626930a014e3
Daisuke Fujita <dtanshi45@gmail.com>
2016-09-16 00:55:57 +0900

    Fetch image and upload it to S3

--- Makefile
+++ Makefile
@@ -1,7 +1,19 @@
 NAME := picam2s3
 LDFLAGS := -ldflags="-s -w"
 
+GLIDE := $(shell command -v glide 2>&1 > /dev/null)
+
 .DEFAULT_GOAL := bin/$(NAME)
 
-bin/$(NAME):
+bin/$(NAME): deps
 	go build $(LDFLAGS) -o bin/$(NAME)
+
+.PHONY: deps
+deps:
+	glide install
+
+.PHONY: glide
+glide:
+ifndef GLIDE
+	curl https://glide.sh/get | sh
+endif
--- main.go
+++ main.go
@@ -1,9 +1,69 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+const Usage = "Usage: picam2s3 <bucket> <url>"
+
+func fetchImage(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("saveImage failed. status code: %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
 func main() {
-	fmt.Println("hoge")
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, Usage)
+		os.Exit(1)
+	}
+
+	bucket := os.Args[2]
+	url := os.Args[3]
+
+	body, err := fetchImage(url)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	key := fmt.Sprintf("%d.jpg", time.Now().Unix())
+
+	svc := s3.New(session.New(), &aws.Config{})
+
+	resp, err := svc.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(body),
+		ACL:    aws.String("private"),
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Println(resp)
 }
